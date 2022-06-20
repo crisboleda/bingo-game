707a9910249665dea4ed19ca08d39501f5f12ec8
Cristhian <casboleda@gmail.com>
2022-06-20 15:25:06 -0500

    feat: API CORS implementation

--- bingo-api/ApiBingo/ApiBingo/settings.py
+++ bingo-api/ApiBingo/ApiBingo/settings.py
@@ -40,9 +40,11 @@ INSTALLED_APPS = [
 
     'rest_framework',
     'bingo_api',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -82,6 +84,11 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+    "http://127.0.0.1:3000"
+]
 
 
 # Password validation

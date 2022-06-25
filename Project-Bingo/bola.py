e786d90e903f2532a9d4b7f2ecb5916db6638982
Cristhian <casboleda@gmail.com>
2022-06-25 18:58:45 -0500

    tests: New test case

--- Project-Bingo/bola.py
+++ Project-Bingo/bola.py
@@ -1,7 +1,7 @@
 class Bola:
-    def __init__(self, letra, numero):
+    def __init__(self, letra: str, numero: int):
         self.letra = letra
         self.numero = numero
 
     def visualizar_bola(self):
-        return "{}{}".format(self.letra, self.numero)
+        return "{}{}".format(self.letra.upper(), self.numero)
--- Project-Bingo/tests/ball_test.py
+++ Project-Bingo/tests/ball_test.py
@@ -6,7 +6,9 @@ class BallTest(TestCase):
     def setUp(self) -> None:
         self.ballot_one = Bola("B", 12)
         self.ballot_two = Bola("I", 20)
+        self.ballot_three = Bola("n", 30)
 
     def test_show_ball(self):
         self.assertEqual("B12", self.ballot_one.visualizar_bola())
         self.assertEqual("I20", self.ballot_two.visualizar_bola())
+        self.assertEqual("N30", self.ballot_three.visualizar_bola())
